11400b4f9c51ee5683dde9d3daf391db02130cc6
Caro2396b <mcarolinabs_2396@hotmail.com>
2025-11-05 10:16:28 -0600

    Add files via upload

---
+++ 12. Grades.py
@@ -0,0 +1,14 @@
+#12. Grades
+## Else
+#An else clause can be optionally added to an if statement.
+
+#If the condition evaluates to True, code in the if part is executed.
+#If the condition evaluates to False, code in the else part is executed.
+
+# Write code below 💖
+grade = float(input("Enter your grade: "))
+
+if grade > 55:
+  print('You passed')
+else:
+    print('You failed')
